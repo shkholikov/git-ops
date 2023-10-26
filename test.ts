32d796ba93aa8f86913ea1c00faea1545fb0644d
Shakhzod Kholikov <51167612+shkholikov@users.noreply.github.com>
2023-10-26 15:00:11 +0200

    Update test.ts

--- test.ts
+++ test.ts
@@ -6,3 +6,40 @@ const handleScroll = () => {
         setShowStickySaveBtn(false);
       }
     };
+
+ <ConfirmationModal
+                isOpen={state.showOnlyUnansweredConfirmation}
+                title="Unbeantwortet anzeigen"
+                message={
+                    state.showOnlyUnanswered
+                        ? "Sind Sie damit einverstanden, dass alle Fragen angezeigt werden?"
+                        : "Sind Sie damit einverstanden, dass nur unbeantwortete Fragen angezeigt werden?"
+                }
+                yesButtonText="Ja"
+                noButtonText="Nein"
+                onYes={getOnlyUnanswered}
+                onNo={() => setState({ ...state, showOnlyUnansweredConfirmation: false })}
+            />
+
+
+
+                      function getOnlyUnanswered() {
+        if (!state.showOnlyUnanswered) {
+            //deep copy
+            let currentAnswerItem = JSON.parse(JSON.stringify(state.answerItem));
+            currentAnswerItem.ANSWERS = currentAnswerItem.ANSWERS.filter((item: IAnswer) => !item.answer && item.questionType === "question");
+            setState((state) => ({
+                ...state,
+                showOnlyUnanswered: !state.showOnlyUnanswered,
+                answerItemWithUnanswered: currentAnswerItem,
+                showOnlyUnansweredConfirmation: false
+            }));
+        } else {
+            setState((state) => ({
+                ...state,
+                showOnlyUnanswered: !state.showOnlyUnanswered,
+                showOnlyUnansweredConfirmation: false
+            }));
+        }
+        void saveData(false);
+    }
